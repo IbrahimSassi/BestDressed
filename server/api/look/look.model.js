a9d9f03bac145872d6db6ede157347cbd9399799
IbrahimSassi <sassiibrahim12@gmail.com>
2016-08-14 23:54:07 +0100

    'AddingUploadingFiles'

--- server/api/look/look.model.js
+++ server/api/look/look.model.js
@@ -2,30 +2,30 @@
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema({
-    image:String,
-    linkUrl:String,
-    title:String,
-    description:String,
-    tags:[{
-        type:String
+    image: String,
+    linkUrl: String,
+    title: String,
+    description: String,
+    tags: [{
+        type: String
     }],
     _creator: {
-        type:Schema.ObjectId,
-        ref:'User'
+        type: Schema.ObjectId,
+        ref: 'User'
     },
-    email:String,
-    createTime:{
-        type:Date,
-        'default':Data.now
+    email: String,
+    createTime: {
+        type: Date,
+        'default': Data.now
     },
-    views:{
-        type:'number',
-        'default':0
+    views: {
+        type: 'number',
+        'default': 0
     },
-    upVotes:{
-        type:'number',
-        'default':0
+    upVotes: {
+        type: 'number',
+        'default': 0
     }
 });
 
-module.exports = mongoose.model('Look',LookSchema);
+module.exports = mongoose.model('Look', LookSchema);
