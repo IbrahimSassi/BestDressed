c0e786406f8aba4d6d1363b10551834c138dc09c
IbrahimSassi <sassiibrahim12@gmail.com>
2016-08-18 14:07:05 +0100

    'AddingComment/Backend+Frontend'

--- client/app/look/look.Ctrl.js
+++ client/app/look/look.Ctrl.js
@@ -5,9 +5,9 @@
     .module('app')
     .controller('LookCtrl', LookCtrl);
 
-  LookCtrl.$inject = ['$scope', '$stateParams', 'looksAPI', 'Auth'];
+  LookCtrl.$inject = ['$scope', '$stateParams', 'looksAPI', 'commentAPI', 'Auth'];
 
-  function LookCtrl($scope, $stateParams, looksAPI, Auth) {
+  function LookCtrl($scope, $stateParams, looksAPI, commentAPI, Auth) {
 
     $scope.user = Auth.getCurrentUser();
     $scope.id = $stateParams.lookId;
@@ -29,6 +29,34 @@
       }, function(err) {
         console.log('failed to get look ', err);
       });
+
+    commentAPI.getComments($scope.id)
+      .then(function(data) {
+        console.log('Comments' , data);
+        $scope.comments = data;
+      }, function(err) {
+        console.log('failed to get comments ', err);
+      });
+
+    // Post new comment
+    $scope.postComment = function() {
+      var comment = {
+        authorId: $scope.user._id,
+        authorName: $scope.user.name,
+        authorEmail: $scope.user.email,
+        gravatar: $scope.user.gravatar,
+        comment: $scope.comment.body,
+        lookId: $scope.id
+      }
+      commentAPI.addComment(comment)
+        .then(function(data) {
+          console.log(data);
+          $scope.comment.body = '';
+          $scope.comments.splice(0, 0, data.data);
+        }, function(err) {
+          console.log('failed to post object ', err);
+        });
+    }
 
   }
 })();
